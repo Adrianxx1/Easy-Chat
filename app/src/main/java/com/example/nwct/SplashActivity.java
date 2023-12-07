89c269dedd0e804a262964ce7981f7f48e9cb315
Adrianxx1 <triaxis12q@gmail.com>
2023-12-07 11:14:54 -0600

    commit 5

---
+++ app/src/main/java/com/example/nwct/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.example.easychat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.easychat.model.UserModel;
+import com.example.easychat.utils.AndroidUtil;
+import com.example.easychat.utils.FirebaseUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if(getIntent().getExtras()!=null){
+            //from notification
+
+                        }
+                    });
+
+
+        }else{
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if(FirebaseUtil.isLoggedIn()){
+                        startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                    }else{
+                        startActivity(new Intent(SplashActivity.this,LoginPhoneNumberActivity.class));
+                    }
+                    finish();
+                }
+            },1000);
+        }
+    }
+}
