b3ef266703fe43258c4c919f8ad2d0fad859207c
Adrianxx1 <triaxis12q@gmail.com>
2023-12-07 11:21:05 -0600

    commit 9

---
+++ app/src/main/java/com/example/nwct/ProfileFragment.java
@@ -0,0 +1,85 @@
+package com.example.easychat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.easychat.model.UserModel;
+import com.example.easychat.utils.AndroidUtil;
+import com.example.easychat.utils.FirebaseUtil;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+
+public class ProfileFragment extends Fragment {
+
+    ImageView profilePic;
+    EditText usernameInput;
+    EditText phoneInput;
+    Button updateProfileBtn;
+    ProgressBar progressBar;
+    TextView logoutBtn;
+
+    UserModel currentUserModel;
+    ActivityResultLauncher<Intent> imagePickLauncher;
+    Uri selectedImageUri;
+
+    public ProfileFragment() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        imagePickLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        if(data!=null && data.getData()!=null){
+                            selectedImageUri = data.getData();
+                            AndroidUtil.setProfilePic(getContext(),selectedImageUri,profilePic);
+                        }
+                    }
+                }
+                );
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
+        profilePic = view.findViewById(R.id.profile_image_view);
+        usernameInput = view.findViewById(R.id.profile_username);
+        phoneInput = view.findViewById(R.id.profile_phone);
+        updateProfileBtn = view.findViewById(R.id.profle_update_btn);
+        progressBar = view.findViewById(R.id.profile_progress_bar);
+        logoutBtn = view.findViewById(R.id.logout_btn);
+
+
+
+
+        });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
